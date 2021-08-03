c4b41954fb0469692748b5c7ea995a05e86ba814
hoon <sotjs12@naver.com>
2021-08-03 22:26:45 +0900

    programers hash, stack 문제 풀이

---
+++ BOJ/Programers.js
@@ -0,0 +1,125 @@
+/*
+https://programmers.co.kr/learn/courses/30/lessons/42578?language=javascript
+
+스파이들은 매일 다른 옷을 조합하여 입어 자신을 위장합니다.
+
+예를 들어 스파이가 가진 옷이 아래와 같고 오늘 스파이가 동그란 안경, 긴 코트, 파란색 티셔츠를 입었다면 다음날은 청바지를 추가로 입거나 동그란 안경 대신 검정 선글라스를 착용하거나 해야 합니다.
+
+종류	이름
+얼굴	동그란 안경, 검정 선글라스
+상의	파란색 티셔츠
+하의	청바지
+겉옷	긴 코트
+스파이가 가진 의상들이 담긴 2차원 배열 clothes가 주어질 때 서로 다른 옷의 조합의 수를 return 하도록 solution 함수를 작성해주세요.
+
+제한사항
+clothes의 각 행은 [의상의 이름, 의상의 종류]로 이루어져 있습니다.
+스파이가 가진 의상의 수는 1개 이상 30개 이하입니다.
+같은 이름을 가진 의상은 존재하지 않습니다.
+clothes의 모든 원소는 문자열로 이루어져 있습니다.
+모든 문자열의 길이는 1 이상 20 이하인 자연수이고 알파벳 소문자 또는 '_' 로만 이루어져 있습니다.
+스파이는 하루에 최소 한 개의 의상은 입습니다.
+입출력 예
+clothes	return
+[["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]	5
+[["crowmask", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]]
+*/
+
+function solution(clothes) {
+    return Object.values(clothes.reduce((ret, [_, key]) => {
+        ret[key] = (ret[key] | 0) + 1;
+        return ret;
+    }, {})).reduce((result, value) => result * (value + 1), 1) - 1;
+}
+
+
+/*
+https://programmers.co.kr/learn/courses/30/lessons/42579
+
+스트리밍 사이트에서 장르 별로 가장 많이 재생된 노래를 두 개씩 모아 베스트 앨범을 출시하려 합니다. 노래는 고유 번호로 구분하며, 노래를 수록하는 기준은 다음과 같습니다.
+
+속한 노래가 많이 재생된 장르를 먼저 수록합니다.
+장르 내에서 많이 재생된 노래를 먼저 수록합니다.
+장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다.
+노래의 장르를 나타내는 문자열 배열 genres와 노래별 재생 횟수를 나타내는 정수 배열 plays가 주어질 때, 베스트 앨범에 들어갈 노래의 고유 번호를 순서대로 return 하도록 solution 함수를 완성하세요.
+
+제한사항
+genres[i]는 고유번호가 i인 노래의 장르입니다.
+plays[i]는 고유번호가 i인 노래가 재생된 횟수입니다.
+genres와 plays의 길이는 같으며, 이는 1 이상 10,000 이하입니다.
+장르 종류는 100개 미만입니다.
+장르에 속한 곡이 하나라면, 하나의 곡만 선택합니다.
+모든 장르는 재생된 횟수가 다릅니다.
+입출력 예
+genres	plays	return
+["classic", "pop", "classic", "classic", "pop"]	[500, 600, 150, 800, 2500]	[4, 1, 3, 0]
+
+*/
+
+function solution(genres, plays) {
+    //괜히 오기부림.. 다음부턴 이러지 말자.
+    return Object
+        .values(genres.reduce((ret, genre, idx) => {
+            if (!ret[genre]) ret[genre] = {
+                sum: 0,
+                genre,
+                playArr: []
+            }
+            ret[genre].sum += plays[idx];
+            ret[genre].playArr.push({ idx, play: plays[idx] });
+            return ret;
+        }, {}))
+        .sort((a, b) => b.sum - a.sum)
+        .reduce((ret, obj) => {
+            obj.playArr.sort((a, b) => (a.play == b.play)
+                ? a.idx - b.idx
+                : b.play - a.play);
+            ret.push(...obj.playArr.slice(0, 2).map(playObj => playObj.idx));
+            return ret
+        }, []);
+}
+
+
+/*
+https://programmers.co.kr/learn/courses/30/lessons/42586?language=javascript
+문제 설명
+프로그래머스 팀에서는 기능 개선 작업을 수행 중입니다. 각 기능은 진도가 100%일 때 서비스에 반영할 수 있습니다.
+
+또, 각 기능의 개발속도는 모두 다르기 때문에 뒤에 있는 기능이 앞에 있는 기능보다 먼저 개발될 수 있고, 이때 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포됩니다.
+
+먼저 배포되어야 하는 순서대로 작업의 진도가 적힌 정수 배열 progresses와 각 작업의 개발 속도가 적힌 정수 배열 speeds가 주어질 때 각 배포마다 몇 개의 기능이 배포되는지를 return 하도록 solution 함수를 완성하세요.
+
+제한 사항
+작업의 개수(progresses, speeds배열의 길이)는 100개 이하입니다.
+작업 진도는 100 미만의 자연수입니다.
+작업 속도는 100 이하의 자연수입니다.
+배포는 하루에 한 번만 할 수 있으며, 하루의 끝에 이루어진다고 가정합니다. 예를 들어 진도율이 95%인 작업의 개발 속도가 하루에 4%라면 배포는 2일 뒤에 이루어집니다.
+입출력 예
+progresses	speeds	return
+[93, 30, 55]	[1, 30, 5]	[2, 1]
+[95, 90, 99, 99, 80, 99]	[1, 1, 1, 1, 1, 1]	[1, 3, 2]
+*/
+
+function solution(progresses = [], speeds = []) {
+    let answer = [];
+    let idx = 0;
+    let completeCnt = 0;
+    function calcRemained(a, b) {
+        return Math.ceil((100 - a) / b);
+    }
+
+    while (idx < progresses.length) {
+        let remained = calcRemained(progresses[idx], speeds[idx]);
+        while (++idx < progresses.length) {
+            if (remained >= calcRemained(progresses[idx], speeds[idx])) {
+                completeCnt++;
+            } else {
+                answer.push(completeCnt + 1);
+                completeCnt = 0;
+                break;
+            }
+        }
+    }
+    answer.push(++completeCnt);
+    return answer;
+}
