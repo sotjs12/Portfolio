fc7b697b09f33e041d12c9bab0ee4e6af789b22d
hoon <sotjs12@naver.com>
2021-08-08 23:39:32 +0900

    generator

--- Handbook/mordernJS.js
+++ Handbook/mordernJS.js
@@ -767,13 +767,50 @@ demoGithubUser();
 
 async function wait() {
     await new Promise(resolve => setTimeout(resolve, 1000));
-  
+
     return 10;
-  }
-  
-  function f() {
+}
+
+function f() {
     // shows 10 after 1 second
     wait().then(result => alert(result));
-  }
-  
-  f();
+}
+
+f();
+
+
+
+let repo = 'javascript-tutorial/en.javascript.info'; // 커밋 정보를 얻어올 GitHub 리포지토리
+
+async function* fetchCommits(repo) {
+    let url = `https://api.github.com/repos/${repo}/commits`;
+    while (url) {
+        const response = await fetch(url, {
+            headers: { 'User-Agent': 'Our script' }
+        });
+        const body = await response.json();
+        let nextPage = response.headers.get("Link").match(/<(.*?)>; rel="next"/);
+        nextPage = nextPage?.[1];
+
+        url = nextPage;
+        for (let commit of body) {
+            yield commit
+        }
+    }
+}
+
+
+(async () => {
+
+    let count = 0;
+
+    for await (const commit of fetchCommits('javascript-tutorial/en.javascript.info')) {
+
+        console.log(commit.author.login);
+
+        if (++count == 10) { // 100번째 커밋에서 멈춥니다.
+            break;
+        }
+    }
+
+})();
